cacf90cda4248055e3005c17c239d1ea0bdd5410
peileiscott <peileiscott@qq.com>
2023-07-27 23:00:58 +0800

    Complete conversion exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.split_once(',') {
+            None => Person::default(),
+            Some((name, _)) if name.is_empty() => Person::default(),
+            Some((name, age)) => {
+                let Ok(age) = age.parse::<usize>() else {
+                    return Person::default();
+                };
+
+                Person {
+                    name: name.to_string(),
+                    age,
+                }
+            }
+        }
     }
 }
 
